f68604fbd7a36f7f20016c57e090b516b6601c2b
Kenji Phang <ken.pcx@outlook.com>
2023-01-14 22:08:40 +0000

    Enabled controls in popup conditionally

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,17 @@
 let youtube_url = "https://www.youtube.com/watch?";
 // Whenever the popup is loaded
-document.addEventListener("DOMContentLoaded", async () => {
-    await chrome.tabs.query({ active: true }, async (tabs) => {
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.tabs.query({ active: true }, (tabs) => {
         let currentTab = tabs[0];
+        // Controls and ui
         let focusModeSwitch = document.getElementById("focusModeSwitch");
+        let energyBarSwitch = document.getElementById("energyBarSwitch");
+        let fasterVideosSwitch = document.getElementById("fasterVideosSwitch");
+        let playbackSpeedMultiplier = document.getElementById("playbackSpeedMultiplier");
         // Checks whether focus mode was enabled previously and
         // set the slider if it was set
         if (currentTab.url?.startsWith(youtube_url)) {
-            await chrome.storage.local.get(["focusMode"], function (result) {
+            chrome.storage.local.get(["focusMode"], function (result) {
                 let focusMode = result.focusMode || false;
                 if (focusMode) {
                     focusModeSwitch?.setAttribute("checked", "");
@@ -16,6 +20,29 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
         else {
             focusModeSwitch?.setAttribute("disabled", "");
+        }
+        // Checks whether energy bar was enabled before and sets it if it was
+        chrome.storage.local.get(["fasterVideos"], function (result) {
+            let energyBar = result.energyBar || false;
+            if (energyBar) {
+                energyBarSwitch?.setAttribute("checked", "");
+            }
+        });
+        // Checks whether faster mode was enabled before and sets it if it was
+        if (currentTab.url?.startsWith(youtube_url)) {
+            chrome.storage.local.get(["fasterVideos"], function (result) {
+                let fasterVideos = result.fasterVideos || false;
+                if (fasterVideos) {
+                    fasterVideosSwitch?.setAttribute("checked", "");
+                }
+                else {
+                    playbackSpeedMultiplier.style.display = "none";
+                }
+            });
+        }
+        else {
+            fasterVideosSwitch?.setAttribute("disabled", "");
+            playbackSpeedMultiplier.style.display = "none";
         }
     });
 });
